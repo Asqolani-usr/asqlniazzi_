81429ee6a6763059b585a6f1fb9d478d5f9d2554
olan <asqolani2@gmail.com>
2025-07-24 19:48:47 +0800

    Set homepage & update remote

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,44 +1,29 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
+import { faPhp ,faFigma } from '@fortawesome/free-brands-svg-icons';
+import { faRobot } from '@fortawesome/free-solid-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
 
 const labelsFirst = [
-    "React",
-    "TypeScript",
-    "JavaScript",
-    "HTML5",
-    "CSS3",
-    "SASS",
-    "Flask",
-    "Python",
-    "SQL",
-    "PostgreSQL",
-    "Postman"
+    "Figma",
+    "Canva",
+    
+    
 ];
 
 const labelsSecond = [
-    "Git",
-    "GitHub Actions",
-    "Docker",
-    "AWS",
-    "Azure",
-    "Linux",
-    "Snowflake",
-    "Pandas",
-    "Selenium",
+    "PHP",
+    "HTML",
+    "CSS",
 ];
 
 const labelsThird = [
     "OpenAI",
     "Groq",
-    "LangChain",
-    "Qdrant",
-    "Hugging Face",
-    "LlamaIndex",
-    "Streamlit",
+    "Claude AI",
+    "Gemini",
 ];
 
 function Expertise() {
@@ -48,9 +33,9 @@ function Expertise() {
             <h1>Expertise</h1>
             <div className="skills-grid">
                 <div className="skill">
-                    <FontAwesomeIcon icon={faReact} size="3x"/>
-                    <h3>Full Stack Web Development</h3>
-                    <p>I have built a diverse array of web applications from scratch using modern technologies such as React and Flask. I have a strong proficiency in the SDLC process and frontend + backend development.</p>
+                    <FontAwesomeIcon icon={faFigma} size="3x"/>
+                    <h3>UI/UX Design</h3>
+                    <p> Specialize in designing intuitive and visually appealing user interfaces using tools like Figma. I create wireframes, interactive prototypes, and high-fidelity mockups that focus on usability and user experience. My design process is driven by user-centered thinking and collaboration with developers to ensure smooth implementation across platforms.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsFirst.map((label, index) => (
@@ -60,9 +45,9 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faDocker} size="3x"/>
-                    <h3>DevOps & Automation</h3>
-                    <p>Once the application is built, I help clients set up DevOps testing, CI/CD pipelines, and deployment automation to support the successful Go-Live.</p>
+                    <FontAwesomeIcon icon={faPhp} size="3x"/>
+                    <h3>Front end development</h3>
+                    <p>I have a strong foundation in front-end development using PHP, HTML, and CSS. I specialize in building responsive and user-friendly web applications with a focus on user experience and accessibility.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsSecond.map((label, index) => (
@@ -72,9 +57,9 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faPython} size="3x"/>
-                    <h3>GenAI & LLM</h3>
-                    <p>Stay relevant in the market by leveraging the latest AI models in your projects. I have professional experience building enterprise grade GenAI-enabled solutions to empower intelligent decision making.</p>
+                    <FontAwesomeIcon icon={faRobot} size="3x"/>
+                    <h3>AI</h3>
+                    <p>Stay relevant in today’s tech landscape by applying the latest AI models to your learning journey. I’ve been diving into GenAI tools like OpenAI and Hugging Face, building small-scale projects to sharpen my understanding of LLMs and enhance my future readiness in the AI field..</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsThird.map((label, index) => (
